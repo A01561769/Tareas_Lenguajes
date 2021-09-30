3e2b1ded1e462ce226a7cc0447c9370ee4916da8
AntonyTC <49083894+A01561769@users.noreply.github.com>
2021-09-29 23:59:20 -0600

    1er Avance Tarea 4
    Solo se ha resuelto el problema 1 de la seccion 1

---
+++ T4/Tarea 4.rkt
@@ -0,0 +1,22 @@
+#lang racket
+;SECCION 1 
+;Funcion que regresa la matriz dada sin la columna especificada por su posición en la matriz
+; n = numero de columna a eliminar
+; matrx = matriz a la que se le eliminara la columna
+(define (elimina-columna n matrx)
+  (if (null? matrx)
+      '()
+      (append (list (elimina n (car matrx))) (elimina-columna n (cdr matrx)))
+      )
+  )
+
+(define (elimina n reng)
+  (cond
+    ((null? reng) '())
+    ((= n 1) (cdr reng))
+    (else (append (list (car reng)) (elimina (- n 1) (cdr reng))))
+      )
+  )
+
+(elimina-columna 3 '((4 0 3 1)(5 1 2 1)(6 0 1 1)))
+;(elimina 3 '(4 0 3 1))
