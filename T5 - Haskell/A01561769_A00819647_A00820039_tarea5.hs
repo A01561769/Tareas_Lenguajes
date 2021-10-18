22984c15fc95cfe4ac0cab75eb4e32e31eeb6302
AntonyTC <49083894+A01561769@users.noreply.github.com>
2021-10-17 19:24:31 -0600

    Ejercicios 1 a 2 listos

--- T5 - Haskell/A01561769_A00819647_A00820039_tarea5.hs
+++ T5 - Haskell/A01561769_A00819647_A00820039_tarea5.hs
@@ -28,8 +28,50 @@ mayor a b c d
   | a < d = mayor d a b c
   | otherwise = a
 
+--  Función recursiva que cuenta la cantidad de números primos
+--  que existen en un rango definido por dos argumentos.
+--  a = limite inferior del rango
+--  b = limite superior del rango
+primos :: Float -> Float -> Int
+primos a b
+    | a > b = 0
+    | a == b = 
+        if verPrim a (a - 1) then
+            1
+        else
+            0
+    | otherwise =
+        if verPrim a (a - 1) then
+            1 + primos (a + 1) b
+        else
+            0 + primos (a + 1) b
+
+--  Función recursiva que verifica si un valor es primo
+--  haciendo la division por cada valor previo
+--  a = valor a dividir
+--  b = divisor
+verPrim :: Float -> Float -> Bool
+verPrim a b
+    | a == 0 = False 
+    | b == 1 = True
+    | verFloat (a / b) = verPrim a (b - 1)
+    | otherwise = False
+
+--  Función para verificar si un valor es Float
+--  x = valor a verificar
+verFloat :: Float -> Bool
+verFloat x = x /= fromInteger (round x)
+
+
+
+
+
 main = do
-    print "1. -"
+    print "1. - Medio"
     print (medio 2 1 5 4)
     print (medio 2 2 2 2)
-    print "2. -"
+    print "2. - Primos"
+    print (primos 1 10)
+    print (primos 5 11)
+    print (primos 8 10)
+    print "3. - Mayores"
